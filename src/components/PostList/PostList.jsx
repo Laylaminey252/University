ff765b48df6ae7cf116305e6fb488a78e71476c0
Laylaminey252 <leylocadow@gmail.com>
2024-07-27 01:04:25 +0400

    eigth commited.

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -15,11 +15,29 @@ function PostList() {
     return <ErorrItem />
   }
   return (
-  <div>
-    <h1>
-      PostList
-    </h1>
-  </div>
+    <section className="py-10 bg-white sm:py-16 lg:py-24">
+      <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div className="max-w-2xl mx-auto text-center">
+          <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
+            Latest from blog
+          </h2>
+          <p className="max-w-xl mx-auto mt-4 text-base leading-relaxed text-gray-600">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis.
+          </p>
+        </div>
+
+        <div className="grid max-w-md grid-cols-1 mx-auto mt-12 lg:max-w-full lg:mt-16 lg:grid-cols-3 gap-x-16 gap-y-12">
+          {posts.map((post) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+
+          {/* <PostItem />
+            <PostItem />
+            <PostItem /> */}
+        </div>
+      </div>
+    </section>
   );
 }
 
